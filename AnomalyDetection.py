e4a33efb263374b158e934258e9161a185c4a3d0
hema veeradhi <hveeradh@redhat.com>
2020-08-26 14:23:28 -0400

    Updating the anomaly detection notebook and python script

--- AnomalyDetection.py
+++ AnomalyDetection.py
@@ -1,17 +1,36 @@
 import os, sys
+import pickle
+
 
 class AnomalyDetection(object):
     def __init__(self):
         print("Initializing...")
-        self.model_file = os.environ.get('MODEL_FILE', 'model.joblib')
+        self.model_file = os.environ.get("MODEL_FILE", "trained_model.pkl")
 
         print("Load modelfile: %s" % (self.model_file))
-        self.clf = load(open(self.model_file, 'rb'))
+        self.clf = pickle.load(open(self.model_file, "rb"))
 
     def predict(self, X, feature_names):
-        print("Predict features: ", X) 
-
         prediction = self.clf.predict(X)
-        print("Prediction: " , prediction)
-        
-        return prediction
+
+        return prediction[["ds", "yhat", "yhat_lower", "yhat_upper"]]
+
+    def forecast_anomalies(self, forecast_df, signal_df):
+        detections = []
+        for y, yhat_upper, yhat_lower in zip(
+            signal_df.y, forecast_df.yhat_upper, forecast_df.yhat_lower
+        ):
+
+            if y <= yhat_lower:
+                detections.append(1)
+
+            elif y >= yhat_upper:
+                detections.append(1)
+
+            else:
+                detections.append(0)
+
+        detected_df = forecast_df
+        detected_df["predicted_anomaly"] = detections
+
+        return detected_df
